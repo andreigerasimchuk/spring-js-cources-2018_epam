908ef20f134b8b9655f242f1b2ab812791ee0e59
andreigerasimchuk <gerasimchuk.grodno@gmail.com>
2018-03-24 21:29:17 +0300

    added test for delete operation

--- terminal/controllers/remove.js
+++ terminal/controllers/remove.js
@@ -3,10 +3,7 @@ const { getAllTodos, writeFile, findCurrentTodo, print, printEror } = require('.
 exports.remove = (id) => {
     getAllTodos()
         .then(todos => {
-            const currentTodo = findCurrentTodo(id, todos);
-            currentTodo.todos.splice(currentTodo.index, 1);
-
-            return currentTodo.todos;
+            return removeTodo(id, todos);
         })
         .then(todos => {
             writeFile(JSON.stringify({ todos }));
@@ -15,4 +12,11 @@ exports.remove = (id) => {
         .catch(err => {
             printEror(err);
         });
+}
+
+exports.removeTodo = (id, todos) => {
+    const currentTodo = findCurrentTodo(id, todos);
+    currentTodo.todos.splice(currentTodo.index, 1);
+
+    return currentTodo.todos;
 }
---
+++ terminal/test.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const { removeTodo } = require('./controllers/remove');
+
+const todos = [
+    {
+        "title": "test1",
+        "description": "test1",
+        "isLiked": true,
+        "comments": ["comment1", "comment1"],
+        "id": "d5621df0-2f4b-11e8-af19-4159c8d704e5"
+    },
+    {
+        "title": "test2",
+        "description": "test2",
+        "isLiked": false,
+        "comments": [],
+        "id": "ad6ce6b0-2f4e-11e8-a6df-3d1a4aa104ba"
+    },
+    {
+        "isLiked": true,
+        "comments": ["comment1", "comment2", "comment3", "comment4"],
+        "id": "e0415520-2f4f-11e8-b501-2524686651e9",
+        "title": "test3",
+        "description": "test 3"
+    }
+];
+
+describe('todo', () => {
+
+    it('remove todo by id', () => {
+        const currentTodos = [...todos];
+        const result = removeTodo('e0415520-2f4f-11e8-b501-2524686651e9', currentTodos);
+        assert.deepEqual(result, [
+            {
+                "title": "test1",
+                "description": "test1",
+                "isLiked": true,
+                "comments": ["comment1", "comment1"],
+                "id": "d5621df0-2f4b-11e8-af19-4159c8d704e5"
+            },
+            {
+                "title": "test2",
+                "description": "test2",
+                "isLiked": false,
+                "comments": [],
+                "id": "ad6ce6b0-2f4e-11e8-a6df-3d1a4aa104ba"
+            }
+        ]);
+    });
+});
+
