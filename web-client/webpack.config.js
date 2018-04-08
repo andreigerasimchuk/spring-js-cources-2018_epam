072912c48741f4b396b78b8792f6314cb01ed384
andreigerasimchuk <gerasimchuk.grodno@gmail.com>
2018-04-08 12:45:19 +0300

    added css/cscc loader

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -2,9 +2,29 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
   }
 };
